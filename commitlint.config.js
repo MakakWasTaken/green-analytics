8515d5a047e0b589558958c3ba70535cd60a2326
Markus Moltke <makakwastaken@gmail.com>
2023-10-26 16:02:27 +0200

    feat: added commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,6 @@
-const emojiPlugin = require('commitlint-plugin-gitmoji')
-
 /**
  * @type {import('@commitlint/types').UserConfig}
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  plugins: [emojiPlugin],
 }
