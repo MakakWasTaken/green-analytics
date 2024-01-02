8822cfe91d729d041f762956cad11b4175dca377
Markus Moltke <markus@unknown-studios.com>
2024-01-02 13:31:18 +0100

    :bug: fix: cors error

--- next.config.mjs
+++ next.config.mjs
@@ -43,6 +43,9 @@ const advancedHeaders = [
 
 const apiHeaders = [
   { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  {
+    key: 'Access-Control-Allow-Origin', value: '*' 
+  },
   { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
   {
     key: 'Access-Control-Allow-Headers',
